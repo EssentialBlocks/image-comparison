53e0cfbb683c2fe45d47f93c236cec3fb902287c
Monir <fencemonir@gmail.com>
2022-12-07 15:10:09 +0600

    update readme & rebuild

--- dist/frontend/index.asset.php
+++ dist/frontend/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '2d8696a8e5eb7674134246987e1df3ee');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '4e21f420ff2589b531b1');
